ae1003e5021bafa42dd7c9307f1b0ce56c5c977d
Dann Bohn <djb44@psu.edu>
2018-02-21 20:52:16 -0500

    cleanup initial cruft commit

--- build/lib/ktpl/kube.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import call, check_call, Popen, PIPE
-
-def run_kube_command(stdin, kube_method):
-    proc = Popen(["kubectl", kube_method, "-f", "-"], stdin=PIPE)
-    # print(stdin)
-    proc.stdin.write(stdin.encode('utf-8'))
-    stdout, stderr = proc.communicate()

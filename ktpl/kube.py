db585bf673ed55a53232a8c99438d6568a753f1b
Dann Bohn <djb44@psu.edu>
2018-03-06 16:19:34 -0500

    adds basic retry logic to allow kube to converge on it's own.

--- ktpl/kube.py
+++ ktpl/kube.py
@@ -1,7 +1,11 @@
 import os
 from subprocess import call, check_call, Popen, PIPE
 
-def run_kube_command(stdin, kube_method):
-    proc = Popen(["kubectl", kube_method, "-f", "-"], stdin=PIPE)
+def run_kube_command(stdin, kube_method, retry=True):
+    proc = Popen(["kubectl", kube_method, "-f", "-"], stdin=PIPE, stderr=PIPE, stdout=PIPE)
     proc.stdin.write(stdin.encode('utf-8'))
     stdout, stderr = proc.communicate()
+    if stderr and retry:
+        run_kube_command(stdin, kube_method, retry=False)
+    if stderr and not retry:
+        print(stderr)

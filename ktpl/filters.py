0f30f6100283a56c0fbfd31ff03c35caf7193373
Dann Bohn <djb44@psu.edu>
2018-03-15 21:33:01 -0400

    updates b64 filters to properly encode, decode strings

--- ktpl/filters.py
+++ ktpl/filters.py
@@ -2,10 +2,12 @@ import base64
 from slugify import slugify
 
 def b64enc(input):
-    return base64.b64encode(input)
+    output = base64.b64encode(input.encode())
+    return output.decode()
 
 def b64dec(input):
-    return base64.b64decode(input)
+    output = base64.b64decode(input.encode())
+    return output.decode()
 
 def slugify_string(input):
     return slugify(input)

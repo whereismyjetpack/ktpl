febf44eb969eb8b7e18de329a9880400daacda1e
Dann Bohn <djb44@psu.edu>
2018-04-02 08:26:45 -0400

    uses tempfile instead of pipe. allows bigger manifests to be rendered at once

--- ktpl/kube.py
+++ ktpl/kube.py
@@ -1,11 +1,18 @@
 import os
 from subprocess import call, check_call, Popen, PIPE
+from tempfile import NamedTemporaryFile
+
 
 def run_kube_command(stdin, kube_method, retry=True):
-    proc = Popen(["kubectl", kube_method, "-f", "-"], stdin=PIPE, stderr=PIPE, stdout=PIPE)
-    proc.stdin.write(stdin.encode('utf-8'))
+    f = NamedTemporaryFile(delete=False)
+    f.write(stdin.encode())
+    f.close()
+    proc = Popen(["kubectl", kube_method, "-f", f.name], stdin=PIPE, stderr=PIPE, stdout=PIPE)
     stdout, stderr = proc.communicate()
+    os.unlink(f.name)
     if stderr and retry:
         run_kube_command(stdin, kube_method, retry=False)
     if stderr and not retry:
         print(stderr)
+    if stdout:
+        print(stdout.decode())
